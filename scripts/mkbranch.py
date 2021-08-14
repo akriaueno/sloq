8dd6c0b671df691750162a1f15c5ac09b29e2f53
akriaueno <akiraueno@outlook.com>
2021-08-14 21:53:34 +0000

    rm unused variable

--- scripts/mkbranch.py
+++ scripts/mkbranch.py
@@ -17,11 +17,9 @@ formatted_issue_title = issue_title.strip().replace(" ", "_")
 branch_base = "feature"
 branch_detail = f"#{issue_no}_{formatted_issue_title}"
 branch_name = f"{branch_base}/{branch_detail}"
-create_branch = False
 while True:
     user_input = input(f"{print_prefix} Create branch {branch_name}?[Y/n]: ")
     if user_input in ["y", "yes", "Y", "YES", "Yes"]:
-        create_branch = True
         break
     elif user_input in ["n", "no", "N", "NO", "No"]:
         exit()
